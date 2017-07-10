b0780559ba1bc083247b9ee14ee87265834fa5e4
Urs Geissbühler <urx@urx.ch>
2017-07-10 20:42:48 +0200

    WIP

--- TermsrvPatcher/MainWindow.xaml.cs
+++ TermsrvPatcher/MainWindow.xaml.cs
@@ -2,6 +2,9 @@
 using System.IO;
 using System.Diagnostics;
 using System.Collections.Generic;
+using System;
+using Microsoft.Win32;
+using System.ServiceProcess;
 
 namespace TermsrvPatcher
 {
@@ -10,131 +13,85 @@ namespace TermsrvPatcher
     /// </summary>
     public partial class MainWindow : Window
     {
-        private string path;
         public MainWindow()
         {
             InitializeComponent();
-            path = Patcher.termsrvPath;
-        }
-
-        private void buttonVersion_Click(object sender, RoutedEventArgs e)
-        {
-            MessageBox.Show(Patcher.getVersion(path));
-        }
-
-        private void buttonChown_Click(object sender, RoutedEventArgs e)
-        {
-            Patcher.takeOwnership(path, Patcher.getAdministratorsIdentity());
+            radioButtonEnableRdp.IsChecked = true;
         }
 
         private void buttonTest_Click(object sender, RoutedEventArgs e)
         {
-            /*// Taking ownership
-            Process.Start("takeown", @"/a /r /f C:\termsrv\").WaitForExit();
-            // Granting Administrators rights
-            Process.Start("ICACLS", @"C:\termsrv\ /Grant *S-1-5-32-544:F /t").WaitForExit();
+            /// <summary>
+            /// Prepares a copy of termsrv.dll for testing the processing of taking ownership, getting full control and patching the file.
+            /// Uses robocopy to prepare a copy of C:\Windows\system32\termsrv.dll in the folder C:\termsrv with exact the same persmissions and ownership for the file and the containing folder.
+            /// </summary>
 
-            Process.Start("robocopy.exe", @"/mir /lev:1 /copyall /secfix c:\windows\system32 C:\termsrv termsrv.*").WaitForExit();*/
+            //RegistryKey key = Registry.LocalMachine.OpenSubKey(@"SYSTEM\CurrentControlSet\Control\Terminal Server", true);
+            // Enable RDP
+            Registry.SetValue(@"HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server", "fDenyTSConnections", 0, RegistryValueKind.DWord);
 
-            patch(@"C:\termsrv\termsrv.dll");
+            // Disable multiple sessions per user
+            Registry.SetValue(@"HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server", "fSingleSessionPerUser", 1, RegistryValueKind.DWord);
+
+            // Disable remote logon for user accounts that are not password protected
+            //Registry.SetValue(@"HKLM\SYSTEM\CurrentControlSet\Control\Lsa", "LimitBlankPasswordUse", 1, RegistryValueKind.DWord);
+
+            string testFolder = @"C:\termsrv";
+            string termsrvPath = Patcher.getTermsrvPath();
+
+            //// Ensure robocopy is able to to re-prepare the folder if the directory already exists with restricted permissions from a previous test
+            // Take ownership of C:\termsrv and everything inside
+            Process.Start("takeown", String.Format(@"/a /r /f {0}\", testFolder)).WaitForExit();
+            // Grant full control for the Administrators group
+            Process.Start("ICACLS", String.Format(@"{0}\ /Grant *S-1-5-32-544:F /t", testFolder)).WaitForExit();
+
+            // Replicate folder C:\Windows\system32\termsrv.dll as C:\termsrv with all permissions, synch only termserv.* files (includes deleting in the target)
+            Process.Start("robocopy.exe", String.Format(@"/mir /lev:1 /copyall /secfix {0} {1} {2}.*", Path.GetDirectoryName(termsrvPath), testFolder, Path.GetFileNameWithoutExtension(termsrvPath))).WaitForExit();
+
+            // Patch the test file
+            Patcher p = new Patcher(Path.Combine(testFolder, Path.GetFileName(termsrvPath)));
+            p.patch(textBoxFind.Text, textBoxReplace.Text);
+
+            ServiceController sc = new ServiceController("TermService");
+            if (sc.Status == ServiceControllerStatus.Running)
+            {
+                sc.Stop();
+            }
+            sc.WaitForStatus(ServiceControllerStatus.Stopped);
+
+            sc.Start();
+            sc.WaitForStatus(ServiceControllerStatus.Running);
         }
 
-        private void patch(string filePath)
+        private void buttonPatch_Click(object sender, RoutedEventArgs e)
         {
-            string backup = filePath + "." + Patcher.getVersion(filePath);
-            if (!File.Exists(backup))
-            {
-                textBlockMessages.Text += "Backup file...";
-                File.Copy(filePath, backup);
-                textBlockMessages.Text += " OK";
-            }
-            Patcher.takeOwnership(filePath, Patcher.getAdministratorsIdentity());
-            Patcher.setFullControl(filePath, Patcher.getAdministratorsIdentity());
-            FileInfo fi = new FileInfo(filePath);
-            long l = fi.Length;
-            byte b;
-            string find = "39 81 3C 06 00 00 0F 84 53 71 02 00";
-            string replace = "B8 00 01 00 00 89 81 38 06 00 00 90";
-            int? firstHex = null; // handles asterisks at beginning
-            List<int> bin = new List<int>();
-            foreach (string hex in find.Split(' '))
-            {
-                if (hex != System.String.Empty)
-                {
-                    if (hex == "*")
-                    {
-                        bin.Add(-1);
-                    }
-                    else
-                    {
-                        if (firstHex == null)
-                        {
-                            // Position of first non-astersik
-                            firstHex = bin.Count;
-                        }
-                        bin.Add(System.Convert.ToInt32(hex, 16));
-                    }
-                }
-            }
-            if (bin.Count == 0)
-            {
-                // Exception
-            }
-            if (firstHex == null)
-            {
-                // Exception here
-            }
+            ServiceController sc = new ServiceController("TermService");
 
-            List<byte> binReplace = new List<byte>();
-            foreach (string hex in replace.Split(' '))
+            if (sc.Status == ServiceControllerStatus.Running)
             {
-                if (hex != System.String.Empty)
-                {
-                    binReplace.Add(System.Convert.ToByte(hex, 16));
-                }
+                sc.Stop();
             }
-            if (binReplace.Count == 0)
-            {
-                // Exception
-            }
+            sc.WaitForStatus(ServiceControllerStatus.Stopped);
 
+            // Patch the real file
+            Patcher p = new Patcher();
+            p.patch(textBoxFind.Text, textBoxReplace.Text);
 
+            sc.Start();
+            sc.WaitForStatus(ServiceControllerStatus.Running);
+        }
 
-            byte[] binFile = File.ReadAllBytes(filePath);
-            int len = binFile.Length;
-            int match = -1;
-            for (int i = firstHex.Value; i < len; i++)
-            {
-                if (binFile[i] == bin[firstHex.Value])
-                {
-                    if (len - i >= 0) // rethink >= 0
-                    {
-                        match = i - firstHex.Value;
-                        for (int ii = 0; ii < bin.Count; ii++)
-                        {
-                            if (bin[ii] != -1 && bin[ii] != binFile[i+ii])
-                            {
-                                match = -1;
-                                break;
-                            }
-                        }
-                    }
-                }
-                if (match > -1)
-                {
-                    break;
-                }
-            }
-            if (match == -1)
-            {
-                // Exception
-            }
+        private void buttonSetRegistry_Click(object sender, RoutedEventArgs e)
+        {
+            //RegistryKey key = Registry.LocalMachine.OpenSubKey(@"SYSTEM\CurrentControlSet\Control\Terminal Server", true);
+            // Enable RDP
+            Registry.SetValue(@"HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server", "fDenyTSConnections", 0, RegistryValueKind.DWord);
 
-            using (BinaryWriter reader = new BinaryWriter(File.Open(filePath, FileMode.Open)))
-            {
-                reader.BaseStream.Seek(match, SeekOrigin.Begin);
-                reader.Write(binReplace.ToArray());
-            }
+            // Disable multiple sessions per user
+            Registry.SetValue(@"HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server", "fSingleSessionPerUser", 1, RegistryValueKind.DWord);
+
+            // Disable remote logon for user accounts that are not password protected
+            Registry.SetValue(@"HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Lsa", "LimitBlankPasswordUse", 1, RegistryValueKind.DWord);
         }
     }
 }
--- TermsrvPatcher/Patcher.cs
+++ TermsrvPatcher/Patcher.cs
@@ -4,43 +4,298 @@ using System.Diagnostics;
 using System.Security.Principal;
 using System.Security.AccessControl;
 using PrivilegeClass;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace TermsrvPatcher
 {
-    static class Patcher
-
+    class Patcher
     {
-        // Be aware that the process must run in 64-bit mode on 64-bit systems (otherwise termserv.dll is only accessible as C:\Windows\Sysnative\termserv.dll)
-        public static string termsrvPath { get; } = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.System), "termsrv.dll");
+        public string termsrvPath { get; }
+        private byte[] termsrvContent;
 
-        public static string getVersion(string filePath)
+        public static string getTermsrvPath()
         {
-            return FileVersionInfo.GetVersionInfo(filePath).ProductVersion;
+            // Be aware that the process must run in 64-bit mode on 64-bit systems (otherwise termserv.dll is only accessible via C:\Windows\Sysnative\termserv.dll)
+            return Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.System), "termsrv.dll");
         }
 
-        public static IdentityReference getAdministratorsIdentity()
+        public Patcher()
+        {
+            termsrvPath = getTermsrvPath();
+            termsrvContent = File.ReadAllBytes(termsrvPath);
+        }
+
+        public Patcher(string path)
+        {
+            termsrvPath = path;
+            termsrvContent = File.ReadAllBytes(termsrvPath);
+        }
+
+        public void patch(string find, string replace)
+        {
+            int[] findArr = strToIntArr(find);
+            int match = findPattern(findArr);
+
+            byte[] replaceArr = strToByteArr(replace);
+            int matchReplace = findPattern(replaceArr);
+
+            string backup = termsrvPath + "." + getVersion();
+            if (!File.Exists(backup))
+            {
+                //textBlockMessages.Text += "Backup file...";
+                File.Copy(termsrvPath, backup);
+                //textBlockMessages.Text += " OK";
+            }
+            takeOwnership(getAdministratorsIdentity());
+            setFullControl(getAdministratorsIdentity());
+            FileInfo fi = new FileInfo(termsrvPath);
+            long l = fi.Length;
+            //string find = "39 81 3C 06 00 00 0F 84 53 71 02 00";
+            //string replace = "B8 00 01 00 00 89 81 38 06 00 00 90";
+            /*int? firstHex = null; // handles asterisks at beginning
+            List<int> findArr = new List<int>();
+            foreach (string hex in find.Split(' '))
+            {
+                if (hex != System.String.Empty)
+                {
+                    if (hex == "*")
+                    {
+                        findArr.Add(-1);
+                    }
+                    else
+                    {
+                        if (firstHex == null)
+                        {
+                            // Position of first non-astersik
+                            firstHex = findArr.Count;
+                        }
+                        findArr.Add(System.Convert.ToInt32(hex, 16));
+                    }
+                }
+            }
+            if (findArr.Count == 0)
+            {
+                // Exception
+            }
+            if (firstHex == null)
+            {
+                // Exception here
+            }*/
+
+            List<byte> binReplace = new List<byte>();
+            foreach (string hex in replace.Split(' '))
+            {
+                if (hex != System.String.Empty)
+                {
+                    binReplace.Add(System.Convert.ToByte(hex, 16));
+                }
+            }
+            if (binReplace.Count == 0)
+            {
+                // Exception
+            }
+
+            /*int match = findPattern(findArr.ToArray());
+
+            if (match == -1)
+            {
+                // Exception
+            }*/
+
+            using (BinaryWriter writer = new BinaryWriter(File.Open(termsrvPath, FileMode.Open)))
+            {
+                writer.BaseStream.Seek(match, SeekOrigin.Begin);
+                writer.Write(binReplace.ToArray());
+            }
+        }
+
+        public int[] strToIntArr(string pattern)
+        {
+            int firstHex = -1;
+            List<int> bin = new List<int>();
+            foreach (string hex in pattern.Split(' '))
+            {
+                if (hex != System.String.Empty)
+                {
+                    if (hex == "*")
+                    {
+                        bin.Add(-1);
+                    }
+                    else
+                    {
+                        if (firstHex == -1)
+                        {
+                            // Position of first non-astersik
+                            firstHex = bin.Count;
+                        }
+                        bin.Add(Convert.ToInt32(hex, 16));
+                    }
+                }
+            }
+            if (firstHex == -1)
+            {
+                return new int[] { };
+            }
+            return bin.ToArray();
+        }
+
+        public byte[] strToByteArr(string pattern)
+        {
+            List<byte> bin = new List<byte>();
+            foreach (string hex in pattern.Split(' '))
+            {
+                if (hex != System.String.Empty)
+                {
+                    bin.Add(Convert.ToByte(hex, 16));
+                }
+            }
+            return bin.ToArray();
+        }
+
+        private int findPattern(int[] searchPattern)
+        {
+            if (searchPattern.Length == 0)
+            {
+                throw new ArgumentException("Search pattern is empty");
+            }
+
+            int firstHexPos = Array.FindIndex(searchPattern, value => value > -1);
+            if (firstHexPos == -1)
+            {
+                throw new ArgumentException("Search pattern only consists of wildcards");
+            }
+
+            int matchPos = -1;
+            for (int searchPos = firstHexPos; searchPos < termsrvContent.Length - searchPattern.Length + firstHexPos; searchPos++)
+            {
+                if (termsrvContent[searchPos] == searchPattern[firstHexPos])
+                {
+                    matchPos = searchPos - firstHexPos;
+                    for (int patternPos = firstHexPos + 1; patternPos < searchPattern.Length - firstHexPos; patternPos++)
+                    {
+                        if (searchPattern[patternPos] != termsrvContent[searchPos + patternPos - firstHexPos] && searchPattern[patternPos] != -1)
+                        {
+                            matchPos = -1;
+                            break;
+                        }
+                    }
+                }
+                if (matchPos > -1)
+                {
+                    break;
+                }
+            }
+
+            return matchPos;
+        }
+
+        private int findPattern(byte[] searchPattern)
+        {
+            if (searchPattern.Length == 0)
+            {
+                throw new ArgumentException("Search pattern is empty");
+            }
+
+            int matchPos = -1;
+            for (int searchPos = 0; searchPos < termsrvContent.Length - searchPattern.Length; searchPos++)
+            {
+                if (termsrvContent[searchPos] == searchPattern[0])
+                {
+                    matchPos = searchPos;
+                    for (int patternPos = 1; patternPos < searchPattern.Length; patternPos++)
+                    {
+                        if (searchPattern[patternPos] != termsrvContent[searchPos + patternPos])
+                        {
+                            matchPos = -1;
+                            break;
+                        }
+                    }
+                }
+                if (matchPos > -1)
+                {
+                    break;
+                }
+            }
+
+            return matchPos;
+        }
+
+        private int _findPattern(int[] pattern)
+        {
+            //var firstHex = pattern.Where(value => value >-1);
+            //pattern.SequenceEqual;
+            //pattern.Take;
+            //pattern.;
+            int firstHexPos = Array.FindIndex(pattern, value => value > -1);
+
+            if (firstHexPos == -1)
+            {
+                throw new Exception();
+            }
+
+            int matchPos;
+            int searchStartPos = firstHexPos;
+            int idx;
+            do
+            {
+                idx = Array.FindIndex(termsrvContent.Skip(searchStartPos).ToArray(), value => value == pattern[firstHexPos]);
+                if (idx == -1)
+                {
+                    // Exception?
+                    matchPos = -1;
+                    break;
+                }
+                searchStartPos += idx;
+                if (searchStartPos + pattern.Length - firstHexPos > termsrvContent.Length) // -1?
+                {
+                    // Exception?
+                    matchPos = -1;
+                    break;
+                }
+                matchPos = searchStartPos - firstHexPos;
+                for (int searchPos = firstHexPos; searchPos < pattern.Length; searchPos++)
+                {
+                    if (pattern[searchPos] != -1 && pattern[searchPos] != termsrvContent[searchStartPos + searchPos])
+                    {
+                        searchStartPos++;
+                        matchPos = -1;
+                        break;
+                    }
+                }
+            } while (matchPos == -1);
+
+            return matchPos;
+        }
+
+        public string getVersion()
+        {
+            return FileVersionInfo.GetVersionInfo(termsrvPath).ProductVersion;
+        }
+
+        public IdentityReference getAdministratorsIdentity()
         {
             return new SecurityIdentifier(WellKnownSidType.BuiltinAdministratorsSid, null);
         }
 
-        public static void takeOwnership(string filePath, IdentityReference identity)
+        public void takeOwnership(IdentityReference identity)
         {
-            FileInfo fi = new FileInfo(filePath);
+            FileInfo fi = new FileInfo(termsrvPath);
             FileSecurity fs = fi.GetAccessControl();
             Privilege p = new Privilege(Privilege.TakeOwnership);
             p.Enable();
             fs.SetOwner(identity);
-            File.SetAccessControl(filePath, fs); //Update the Access Control on the File
+            File.SetAccessControl(termsrvPath, fs); //Update the Access Control on the File
             p.Revert();
         }
 
-        public static void setFullControl(string filePath, IdentityReference identity)
+        public void setFullControl(IdentityReference identity)
         {
-            FileInfo fi = new FileInfo(filePath);
+            FileInfo fi = new FileInfo(termsrvPath);
             FileSecurity fs = fi.GetAccessControl();
 
             fs.SetAccessRule(new FileSystemAccessRule(identity, FileSystemRights.FullControl, AccessControlType.Allow));
-            File.SetAccessControl(filePath, fs);
+            File.SetAccessControl(termsrvPath, fs);
         }
     }
 }
